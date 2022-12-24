3f5037078a83b05f581718aaa75c35c0f49fa08d
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2022-12-24 22:04:06 +0530

    product name and prices

--- ProductFilterSearch_JS/scripts.js
+++ ProductFilterSearch_JS/scripts.js
@@ -1,49 +1,49 @@
 let products = {
   data: [
     {
-      productName: "Women Solid Longline Top",
+      productName: "Women BTS Top",
       category: "Topwear",
       price: "349",
       image: "images/top6.png",
     },
     {
-      productName: "",
+      productName: "ResPro Waterproof Jacket",
       category: "Jacket",
       price: "1699",
       image: "images/j1.png",
     },
     {
-      productName: "",
+      productName: "Neo Cargo Pants",
       category: "Bottomwear",
       price: "313",
       image: "images/b7.png",
     },
     {
-      productName: "",
+      productName: "Crafty Men's Jacket",
       category: "Jacket",
       price: "2199",
       image: "images/j2.png",
     },
     {
-      productName: "",
+      productName: "Leather Strap Watch",
       category: "Watch",
       price: "1190",
       image: "images/w1.png",
     },
     {
-      productName: "",
+      productName: "Stressed Jeans",
       category: "Bottomwear",
       price: "313",
       image: "images/b5.png",
     },
     {
-      productName: "",
+      productName: "Casual Solid Black Trouser ",
       category: "Bottomwear",
       price: "313",
       image: "images/b4.png",
     },
     {
-      productName: "",
+      productName: "Black Leather Strap Watch",
       category: "Watch",
       price: "1190",
       image: "images/w2.png",
@@ -57,53 +57,53 @@ let products = {
     {
       productName: "Women Casual Dress",
       category: "Topwear",
-      price: "349",
+      price: "1049",
       image: "images/top3.png",
     },
     {
-      productName: "",
+      productName: "Pransh Breathable Jacket",
       category: "Jacket",
       price: "2199",
       image: "images/j3.png",
     },
     {
-      productName: "",
+      productName: "Espr Jacket with Hoodie",
       category: "Jacket",
       price: "2199",
       image: "images/j4.png",
     },
     {
-      productName: "",
+      productName: "Blue Breathable Trouser",
       category: "Bottomwear",
       price: "313",
       image: "images/b1.png",
     },
     {
-      productName: "",
+      productName: "Krihtan Distressed Jeans",
       category: "Bottomwear",
       price: "313",
       image: "images/b6.png",
     },
     {
-      productName: "",
+      productName: "Fancy Lounge Trouser",
       category: "Bottomwear",
       price: "313",
       image: "images/b2.png",
     },
     {
-      productName: "",
+      productName: "Plaine Pants",
       category: "Bottomwear",
       price: "313",
       image: "images/b3.png",
     },
     {
-      productName: "",
+      productName: " Leather Strap Watch",
       category: "Watch",
       price: "1190",
       image: "images/w3.png",
     },
     {
-      productName: "",
+      productName: "Balor Motorcycle Jacket",
       category: "Jacket",
       price: "2199",
       image: "images/j5.png",
@@ -121,25 +121,25 @@ let products = {
       image: "images/top5.png",
     },
     {
-      productName: "",
+      productName: "Custom Watch",
       category: "Watch",
       price: "1190",
       image: "images/w4.png",
     },
     {
-      productName: "Women Solid Dress",
+      productName: "Women Solid Polka Dress",
       category: "Topwear",
-      price: "349",
+      price: "949",
       image: "images/top1.png",
     },
     {
-      productName: "Women Solid Dress",
+      productName: "Women Solid Longline Top",
       category: "Topwear",
       price: "349",
       image: "images/top7.png",
     },
     {
-      productName: "Women Solid Dress",
+      productName: "Unisex T-shirts",
       category: "Topwear",
       price: "349",
       image: "images/top8.png",
@@ -159,5 +159,19 @@ for (let i of products.data) {
   imgContainer.appendChild(image);
   card.appendChild(imgContainer);
 
+  //Container
+  let container = document.createElement("div");
+  container.classList.add("container");
+  // Product name
+  let name = document.createElement("h4");
+  name.classList.add("product-name");
+  name.innerText = i.productName.toUpperCase();
+  container.appendChild(name);
+  // Price
+  let price = document.createElement("h6");
+  price.innerText = "₹" + i.price;
+  container.appendChild(price);
+
+  card.appendChild(container);
   document.getElementById("products").appendChild(card);
 }
