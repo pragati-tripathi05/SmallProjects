0378e23731dc975e7275ac47cd86c9b130466533
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2022-12-25 16:28:37 +0530

    styling menu items

--- CurvedSideMenu/script.js
+++ CurvedSideMenu/script.js
@@ -5,3 +5,14 @@ let navigation = document.querySelector(".navigation");
 menuToggle.onclick = function () {
   navigation.classList.toggle("active");
 };
+
+// Making clicked item as 'active'
+let list = document.querySelectorAll(".list");
+
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+
+  this.classList.add("active");
+}
+
+list.forEach((item) => item.addEventListener("click", activeLink));
