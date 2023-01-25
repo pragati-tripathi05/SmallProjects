eba4102b174e35f7795bf12409774ee27b6a0ade
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2023-01-25 21:16:41 +0530

    react responsive navbar

---
+++ Navbars/react-responsive-navbar/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import "./HomeStyles.css";
+
+function Home() {
+  return (
+    <>
+      <div className="home">
+        <h1>React Navbar</h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
