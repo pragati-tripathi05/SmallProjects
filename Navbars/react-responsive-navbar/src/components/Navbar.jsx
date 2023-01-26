b9cfb391939fe2b9bff2b1b5ce70c0f665bf32cb
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2023-01-26 14:11:16 +0530

    react responsive navbar

--- Navbars/react-responsive-navbar/src/components/Navbar.jsx
+++ Navbars/react-responsive-navbar/src/components/Navbar.jsx
@@ -5,7 +5,22 @@ import "./NavbarStyles.css";
 
 const Navbar = () => {
   return (
-    <div></div>
+    <>
+        <nav>
+            <a href="#">
+                <img src="http://img.logoipsum.com/247.svg" alt=""/>
+            </a>
+            <div>
+                <ul>
+                    <li> <a href="">About</a> </li>
+                    <li> <a href="">Tech Stacks</a> </li>
+                    <li> <a href="">Github Stats</a> </li>
+                    <li> <a href="">Projects</a> </li>
+                    <li> <a href="">Contact</a> </li>
+                </ul>
+            </div>
+        </nav>
+    </>
   )
 }
 
