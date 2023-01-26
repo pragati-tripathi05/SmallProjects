cdd8973ac6894ba7ff435871a843ebfee42f0e9e
PRAGATI TRIPATHI <mailpragatihere@gmail.com>
2023-01-26 15:29:25 +0530

    react responsive navbar

--- Navbars/react-responsive-navbar/src/components/Navbar.jsx
+++ Navbars/react-responsive-navbar/src/components/Navbar.jsx
@@ -1,27 +1,62 @@
-import React from 'react';
+import React from "react";
 import { Component } from "react";
 import "./NavbarStyles.css";
 
+class Navbar extends Component {
+    state = {clicked: false};
 
-const Navbar = () => {
-  return (
-    <>
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+  render() {
+    return (
+      <>
         <nav>
-            <a href="#">
-                <img src="http://img.logoipsum.com/247.svg" alt=""/>
-            </a>
-            <div>
-                <ul>
-                    <li> <a href="">About</a> </li>
-                    <li> <a href="">Tech Stacks</a> </li>
-                    <li> <a href="">Github Stats</a> </li>
-                    <li> <a href="">Projects</a> </li>
-                    <li> <a href="">Contact</a> </li>
-                </ul>
-            </div>
+          <a href="#">
+            <img src="http://img.logoipsum.com/247.svg" alt="" />
+          </a>
+
+          <div>
+            <ul id="navbar" className={this.state.clicked ? "#navbar active" : "navbar"}>
+              <li>
+                {" "}
+                <a href="#" className="active">
+                  Home
+                </a>{" "}
+              </li>
+              <li>
+                {" "}
+                <a href="#">About</a>{" "}
+              </li>
+              <li>
+                {" "}
+                <a href="#">Tech Stacks</a>{" "}
+              </li>
+              <li>
+                {" "}
+                <a href="#">Github Stats</a>{" "}
+              </li>
+              <li>
+                {" "}
+                <a href="#">Projects</a>{" "}
+              </li>
+              <li>
+                {" "}
+                <a href="#">Contact</a>{" "}
+              </li>
+            </ul>
+          </div>
+
+          <div id="mobile" onClick={this.handleClick}>
+            <i
+              id="bar"
+              className={this.clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
         </nav>
-    </>
-  )
+      </>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
